12ce82b27534df0d856826d1121a505f9cf2452c
NgoTanLoi01 <ngotanloidh2018@gmail.com>
2024-06-23 11:39:49 +0700

    feat: config use css, sass

--- src/components/Button/index.js
+++
@@ -1,7 +0,0 @@
-import myAlert from "~/myAlert";
-
-function Button() {
-  return <button onClick={myAlert}>Click me!</button>;
-}
-
-export default Button; // Export component, không phải kết quả của Button()
