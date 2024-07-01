85380a521b481106063a0a7d259a222fb1072710
NgoTanLoi01 <ngotanloidh2018@gmail.com>
2024-07-01 17:57:07 +0700

    feat: build logic header part 1

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,14 +1,15 @@
-import classNames from 'classnames/bind';
-import styles from './AccountItem.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import classNames from 'classnames/bind';
+import Image from '~/components/Image';
+import styles from './AccountItem.module.scss';
 
 const cx = classNames.bind(styles);
 
 function AccountItem() {
     return (
         <div className={cx('wrapper')}>
-            <img
+            <Image
                 className={cx('avatar')}
                 src="https://p16-sign-useast2a.tiktokcdn.com/tos-useast2a-avt-0068-giso/f68a312ef26213fda69ec4342b4d6848~c5_100x100.jpeg?lk3s=a5d48078&nonce=86563&refresh_token=ed5213fc80a4b5cd74948def3b984b08&x-expires=1719910800&x-signature=GpLU0aube804BPz%2B7eYgWpoPUok%3D&shp=a5d48078&shcp=81f88b70"
                 alt="Hoa"
