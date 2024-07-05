915ea3ce8fa912dfdae5880e6bc5d551b45f01dc
NgoTanLoi01 <ngotanloidh2018@gmail.com>
2024-07-05 12:44:56 +0700

    refactor: fix and complete Header part 2

--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -53,7 +53,7 @@ function Menu({ children, items = [], onChange = defaultFn }) {
                                 }}
                             />
                         )}
-                        {renderItems()}
+                        <div className={cx('menu-body')}>{renderItems()}</div>
                     </PopperWrapper>
                 </div>
             )}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h2>Home page</h2>
+    return <h2 style={{ height: 2000 }}>Home page</h2>;
 }
 
 export default Home;
